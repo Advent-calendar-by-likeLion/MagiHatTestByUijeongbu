8bb07da042c70023ba7240792d3dd227e702cabb
minsu.rob <minsu.rob@gmail.com>
2023-03-26 01:34:58 +0900

    fix: 기본 폰트 figma에 맞추어 변경

--- src/styles/layout.js
+++ src/styles/layout.js
@@ -21,4 +21,21 @@ export const GlobalStyle = createGlobalStyle`
     body {
         margin: 0px;
     }
+
+    @font-face {
+        font-family: 'GmarketSansMedium';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    * {
+    box-sizing: border-box;
+    font-family: 'GmarketSansMedium';
+    }
+    div {
+    box-sizing: border-box;
+    font-family: 'GmarketSansMedium';
+    }
+    
 `
