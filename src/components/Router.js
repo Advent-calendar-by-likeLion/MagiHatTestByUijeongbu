80aeca1a8b902e70f28e4ebeda5f72022075a78f
seocylucky <seocylucky@github.com>
2023-03-25 13:35:21 +0900

    add: dashboard 추가를 위한 세팅

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,29 @@
-import { HashRouter as Router, Route, Switch /*, Redirect*/ } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Home from "routes/Home";
 import Profile from "routes/Profile"
 import Write from "../routes/Write";
 import Home2 from "../routes/Home2";
-import AdminPage from "../routes/AdminPage";
+import Admin from "../routes/Admin";
 import WriteSuccess from "../routes/WriteSuccess";
+
+// import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../components/assets/css/animate.min.css";
+import "../components/assets/scss/light-bootstrap-dashboard-react.scss";
+import "../components/assets/css/demo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import AdminLayout from "../routes/Admin.js";
 
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/admin">
-          <AdminPage />
+        <Route exact path="/admin/dashboard">
+          {/* <Admin /> */}
+          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+          <Redirect from="/" to="/admin/dashboard" />
         </Route>
         <Route exact path="/home2">
           <Home2 />
