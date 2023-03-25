d8600293ad0261ce18d7b491e0d503602e445dcc
seocylucky <seocylucky@github.com>
2023-03-25 13:51:11 +0900

    add: dashboard route 파일

---
+++ src/routes.js
@@ -0,0 +1,70 @@
+import Dashboard from "./components/Dashboard/views/Dashboard.js";
+import UserProfile from "./components/Dashboard/views/UserProfile.js";
+import TableList from "./components/Dashboard/views/TableList.js";
+import Typography from "./components/Dashboard/views/Typography.js";
+import Icons from "./components/Dashboard/views/Icons.js";
+import Maps from "./components/Dashboard/views/Maps.js";
+import Notifications from "./components/Dashboard/views/Notifications.js";
+import Upgrade from "./components/Dashboard/views/Upgrade.js";
+
+const dashboardRoutes = [
+  // {
+  //   upgrade: true,
+  //   path: "/upgrade",
+  //   name: "Upgrade to PRO",
+  //   icon: "nc-icon nc-alien-33",
+  //   component: Upgrade,
+  //   layout: "/admin"
+  // },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-chart-pie-35",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  // {
+  //   path: "/user",
+  //   name: "User Profile",
+  //   icon: "nc-icon nc-circle-09",
+  //   component: UserProfile,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/table",
+  //   name: "Table List",
+  //   icon: "nc-icon nc-notes",
+  //   component: TableList,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/typography",
+  //   name: "Typography",
+  //   icon: "nc-icon nc-paper-2",
+  //   component: Typography,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: "nc-icon nc-atom",
+  //   component: Icons,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: "nc-icon nc-pin-3",
+  //   component: Maps,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/notifications",
+  //   name: "Notifications",
+  //   icon: "nc-icon nc-bell-55",
+  //   component: Notifications,
+  //   layout: "/admin"
+  // }
+];
+
+export default dashboardRoutes;
