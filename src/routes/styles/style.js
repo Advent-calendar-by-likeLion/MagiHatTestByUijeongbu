173c340a7c4cfb3787044ec42e903ea8518276d0
seocylucky <seocylucky@github.com>
2023-03-25 13:43:31 +0900

    fix: MediaDiv width 수정 및 mt 수정

--- src/routes/styles/style.js
+++ src/routes/styles/style.js
@@ -4,7 +4,6 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 125px;
 `
 export const HotelImg = styled.img`
     width: 180px;
--- src/styles/layout.js
+++ src/styles/layout.js
@@ -5,7 +5,7 @@ import reset from "styled-reset"
 export const MediaDiv = styled.div`
     margin: 0px auto;
     min-height: calc(var(--vh, 1vh) * 100);
-    width: 768px;
+    width: 100%;
     margin: 0 auto;
     background-color: #FCF4E9;
     display: flex;
