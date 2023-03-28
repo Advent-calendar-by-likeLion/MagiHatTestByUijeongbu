fb8daa2d8732537a090ab929060d96926c1ca80e
minsu.rob <minsu.rob@gmail.com>
2023-03-28 23:41:15 +0900

    fix: 메인 홈 로고 사이즈 반응형 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,8 +13,7 @@ const Home = () => {
   return (
        <BackgroundImg>
           <HomeMent1>나에게 어울리는 문화도시 의정부 사업은?</HomeMent1>
-          <MainCharacterImg className="col-lg-5 col-8 mx-auto " src={MainCharacter} />
-          <RedButton className="mt-5" onClick={toQNA}>찾으러 가기</RedButton>
+          <MainCharacterImg className="col-lg-8 col-12  mx-auto " src={MainCharacter} />
           <ButtonWrapper onClick={toQNA}>찾으러 가기</ButtonWrapper>
       </BackgroundImg>
   )
