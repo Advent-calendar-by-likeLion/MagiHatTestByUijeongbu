85d6df429e1ca696de06d8b5c9bb39bde9d5594a
minsu.rob <minsu.rob@gmail.com>
2023-03-28 22:53:43 +0900

    fix: 메인 레이아웃 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,15 +11,25 @@ const Home = () => {
     }
 
   return (
-      <>
+       <BackgroundImg>
           <HomeMent1>나에게 어울리는 문화도시 의정부 사업은?</HomeMent1>
           <MainCharacterImg className="col-lg-5 col-8 mx-auto " src={MainCharacter} />
-          <RedButton className="mt-5" onClick={toWrite}>찾으러 가기</RedButton>
-      </>
+          <RedButton className="mt-5" onClick={toQNA}>찾으러 가기</RedButton>
+      </BackgroundImg>
   )
 }
 
 export default Home;
+
+const BackgroundImg = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 0px 50px;
+  text-align: center;
+`;
 
 
 const HomeMent1 = styled.div`
--- src/styles/layout.js
+++ src/styles/layout.js
@@ -3,16 +3,16 @@ import styled, { createGlobalStyle } from 'styled-components';
 import reset from "styled-reset"
 
 //background-color: #F5F5F5;
-export const MediaDiv = styled.div`
+    export const MediaDiv = styled.div`
     margin: 0px auto;
     min-height: calc(var(--vh, 1vh) * 100);
-    width: 100%;
+    width: 768px;
     margin: 0 auto;
-    background-color: #df485dd1;
+    background-color: #f5f5f5;
     display: flex;
     flex-direction: column;
     @media screen and (max-width: 768px) {
-        width: 100%;
+    width: 100%;
     }
 `;
 
