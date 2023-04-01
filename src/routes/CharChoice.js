2d8037567462370bad2a6ca0bce7a2762129c462
seocylucky <seocylucky@github.com>
2023-04-01 13:35:19 +0900

    feat: 캐릭터 선택 텍스트 추가 및 애니메이션 작업

--- src/routes/CharChoice.js
+++ src/routes/CharChoice.js
@@ -78,56 +78,74 @@ const CharChoice = () => {
                     <TextDiv>내</TextDiv>
                     <TextDivBold>캐릭터</TextDivBold>
                     <TextDiv>를</TextDiv>
+                    <TextDiv style={{marginLeft: '8px'}}>골라줘!</TextDiv>
                 </div>
-                <TextDiv>골라줘!</TextDiv>
             </div>
-            <div style={{display: 'flex', flexDirection: 'column', gap:'25px', alignItem: 'center', marginTop:'100px', justifyContent: 'center'}}>
                     {
                         char[0] == "존중이" ?
                         <>
+                        <TextBox>존중이 (Respect)</TextBox>
+                        <TextBox2>#존중 #신뢰 #믿음</TextBox2>
+                        <TextBox3>현명하고 지혜로운 성격</TextBox3>
+                        <div style={{display: 'flex', flexDirection: 'column', gap:'25px', alignItem: 'center', justifyContent: 'center'}}>
                         <img src={Respect} alt='존중이' width='150.3px' style={{margin: '0 auto'}}></img>
                         <div style={{display: 'flex', gap: '20px', justifyContent: 'center'}}>
-                            <img src={Dream} alt='상상이' onClick={onDream} style={{opacity: '80%'}}></img>
-                            <img src={Expansion} alt='성장이' onClick={onExpansion} style={{opacity: '80%'}}></img>
-                            <img src={Happy} alt='행복이' onClick={onHappy} style={{opacity: '80%'}}></img>
+                            <CharImg src={Dream} alt='상상이' onClick={onDream} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Expansion} alt='성장이' onClick={onExpansion} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Happy} alt='행복이' onClick={onHappy} style={{opacity: '70%'}}></CharImg>
                         </div>
                         <ButtonWrapper style={{margin: '0 auto', marginTop: '48px'}} onClick={charclick}>선택하기</ButtonWrapper>
+                        </div>
                         </>
                         : 
                         char[0] == "상상이" ?
                         <>
+                        <TextBox>상상이 (Dream)</TextBox>
+                        <TextBox2>#상상 #활기 #모험</TextBox2>
+                        <TextBox3>호기심 많은 성격</TextBox3>
+                        <div style={{display: 'flex', flexDirection: 'column', gap:'25px', alignItem: 'center', justifyContent: 'center'}}>
                         <img src={Dream} alt='상상이' width='150.3px' style={{margin: '0 auto'}}></img>
                         <div style={{display: 'flex', gap: '20px', justifyContent: 'center'}}>
-                            <img src={Respect} alt='존중이' onClick={onRespect} style={{opacity: '80%'}}></img>
-                            <img src={Expansion} alt='성장이' onClick={onExpansion} style={{opacity: '80%'}}></img>
-                            <img src={Happy} alt='행복이' onClick={onHappy} style={{opacity: '80%'}}></img>
+                            <CharImg src={Respect} alt='존중이' onClick={onRespect} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Expansion} alt='성장이' onClick={onExpansion} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Happy} alt='행복이' onClick={onHappy} style={{opacity: '70%'}}></CharImg>
                         </div>
                         <ButtonWrapper style={{margin: '0 auto', marginTop: '48px'}} onClick={charclick}>선택하기</ButtonWrapper>
+                        </div>
                         </>
                         :
                         char[0] == "성장이" ?
                         <>
+                        <TextBox>성장이 (Expansion)</TextBox>
+                        <TextBox2>#성장 #도약 #역량</TextBox2>
+                        <TextBox3>리더쉽 있고 진취적인 성격</TextBox3>
+                        <div style={{display: 'flex', flexDirection: 'column', gap:'25px', alignItem: 'center', justifyContent: 'center'}}>
                         <img src={Expansion} alt='성장이' width='150.3px' style={{margin: '0 auto'}}></img>
                         <div style={{display: 'flex', gap: '20px', justifyContent: 'center'}}>
-                            <img src={Respect} alt='존중이' onClick={onRespect} style={{opacity: '80%'}}></img>
-                            <img src={Dream} alt='상상이' onClick={onDream} style={{opacity: '80%'}}></img>
-                            <img src={Happy} alt='행복이' onClick={onHappy} style={{opacity: '80%'}}></img>
+                            <CharImg src={Respect} alt='존중이' onClick={onRespect} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Dream} alt='상상이' onClick={onDream} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Happy} alt='행복이' onClick={onHappy} style={{opacity: '70%'}}></CharImg>
                         </div>
                         <ButtonWrapper style={{margin: '0 auto', marginTop: '48px'}} onClick={charclick}>선택하기</ButtonWrapper>
+                        </div>
                         </>
                         :
                         <>
+                        <TextBox>행복이 (Happy)</TextBox>
+                        <TextBox2>#행복 #희망 #순수</TextBox2>
+                        <TextBox3>긍정적인 성격</TextBox3>
+                        <div style={{display: 'flex', flexDirection: 'column', gap:'25px', alignItem: 'center', justifyContent: 'center'}}>
                         <img src={Happy} alt='햄복이' width='150.3px' style={{margin: '0 auto'}}></img>
                         <div style={{display: 'flex', gap: '20px', justifyContent: 'center'}}>
-                            <img src={Respect} alt='존중이' onClick={onRespect} style={{opacity: '80%'}}></img>
-                            <img src={Dream} alt='상상이' onClick={onDream} style={{opacity: '80%'}}></img>
-                            <img src={Expansion} alt='성장이' onClick={onExpansion} style={{opacity: '80%'}}></img>
+                            <CharImg src={Respect} alt='존중이' onClick={onRespect} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Dream} alt='상상이' onClick={onDream} style={{opacity: '70%'}}></CharImg>
+                            <CharImg src={Expansion} alt='성장이' onClick={onExpansion} style={{opacity: '70%'}}></CharImg>
                         </div>
                         <ButtonWrapper style={{margin: '0 auto', marginTop: '48px'}} onClick={charclick}>선택하기</ButtonWrapper>
+                        </div>
                         </>
                     }
             </div>
-        </div>
     </>
     )}
 
@@ -174,4 +192,65 @@ const TextDivBold = styled.div`
     text-align: center;
     letter-spacing: -0.02em;
     padding-left: 9px;
+`
+
+const CharImg = styled.img`
+    :hover {
+        transform: scale(1.3);
+        transition: transform .5s;
+        opacity: 1;
+    }
+`
+const TextBox = styled.div`
+    background-color: white;
+    border: 1px solid #000000;
+    border-radius: 3px;
+    width: fit-content;
+    padding: 10px;
+    padding-left: 15px;
+    padding-right: 15px;
+    text-align: center;
+    position: relative;
+    margin-top: 39px;
+    left: 32%;
+
+    @media screen and (max-width: 768px) {
+        left: 14%;
+    }
+`
+
+const TextBox2 = styled.div`
+    background-color: white;
+    border: 1px solid #000000;
+    border-radius: 3px;
+    width: fit-content;
+    padding: 7px;
+    padding-left: 16px;
+    padding-right: 16px;
+    text-align: center;
+    position: relative;
+    font-size: 13px;
+    left: 49%;
+
+    @media screen and (max-width: 768px) {
+        left: 48%;
+    }
+`
+
+const TextBox3 = styled.div`
+    background-color: white;
+    border: 1px solid #000000;
+    border-radius: 3px;
+    width: fit-content;
+    padding: 7px;
+    padding-left: 16px;
+    padding-right: 16px;
+    text-align: center;
+    position: relative;
+    font-size: 13px;
+    left: 44%;
+
+    @media screen and (max-width: 768px) {
+        left: 38%;
+    } 
 `
