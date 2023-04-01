b3674f640424ad5e15116caa86164177c979e390
seocylucky <seocylucky@github.com>
2023-04-01 15:27:31 +0900

    feat: 캐릭터별 로딩 페이지 작업 및 안맞는 사이즈 수정

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,14 +3,29 @@ import Dream from "./assets/svg/loadingChar/1.Dream.svg";
 import Respect from "./assets/svg/loadingChar/2.Respect.svg";
 import Expansion from "./assets/svg/loadingChar/3.Expansion.svg";
 import Happy from "./assets/svg/loadingChar/4.Happy.svg";
+import { useRecoilState } from 'recoil';
+import { userState } from '../recoil/CharState';
 
 
 // 유동적으로 이미지 삽입 필요
 const Loading = ({ }) =>{
+  const [char, setChar] = useRecoilState(userState);
+
     return(
         <>
             <LoadingWrapper>
-              <LoadingImg src={Happy}/>
+              {
+                char[0] == "존중이" ?
+                <LoadingImg src={Respect}/>
+                :
+                char[0] == "상상이" ?
+                <LoadingImg src={Dream}/>
+                :
+                char[0] == "성장이" ?
+                <LoadingImg src={Expansion}/>
+                :
+                <LoadingImg src={Happy}/>
+              }
             </LoadingWrapper>
         </>
     );
@@ -28,7 +43,14 @@ const LoadingWrapper = styled.div `
 `;
 
 const LoadingImg = styled.img`
+  width: 211.22px;
+  height: 228.87px;
+  margin-top: 270px;
+
+@media screen and (max-width: 768px) {
   margin-top:50%;
   width: 50%;
   height: 50%;
+}
+
 `;
