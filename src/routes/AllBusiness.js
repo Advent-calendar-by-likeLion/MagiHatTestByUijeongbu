9549c47d23ec1518b20b1bded0ea8538d0e94857
seocylucky <seocylucky@github.com>
2023-04-01 23:41:50 +0900

    add: 모든 사업 페이지 상단 캐릭터 아이콘 추가 및 클릭했을 시 해당 사업페이지로 이동할 수 있게 작업

--- src/routes/AllBusiness.js
+++ src/routes/AllBusiness.js
@@ -6,6 +6,8 @@ import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 import resultData from '../components/assets/data/resultData';
+import { useRecoilState } from 'recoil';
+import { userState } from '../recoil/CharState';
 
 import business01 from "../components/assets/svg/business/01.svg"
 import business02 from "../components/assets/svg/business/02.svg"
@@ -24,6 +26,10 @@ import business14 from "../components/assets/svg/business/14.svg"
 import business15 from "../components/assets/svg/business/15.svg"
 import business16 from "../components/assets/svg/business/16.svg"
 
+import Respect from '../assets/uijeongbu/character/respect.svg'
+import Dream from '../assets/uijeongbu/character/dream.svg'
+import Happy from '../assets/uijeongbu/character/happy.svg'
+import Expansion from '../assets/uijeongbu/character/expansion.svg'
 
 const suggestImg = new Array(
   business01, //index 0
@@ -44,17 +50,55 @@ const suggestImg = new Array(
   business15,
   business16,
 )
-const AllBusiness = ({}) =>{
+
+const mbtiMatch = {
+  "INTJ" : 1, // 문화도시 콜로키움
+  "ESTJ" : 2, // 333문화쌀롱
+  "ESFP" : 3, // 횡단자캠프
+  "ENTJ" : 4, // 협력활동가 네트워크
+  "ESTP" : 5, // 시민문화활동가 지원
+  "ENFP" : 6, // 문화도시 실험실
+  "INTP" : 7, // 문화자치학교
+  "ISTJ" : 8, // 문화시민 동네연구
+  "ISTP" : 9, // 경기북부 작은연구
+  "ENFJ" : 10, // 백만원 실험실
+  "INFP" : 11, // 문화도시 에디터
+  "ENTP" : 12, // U+ 스팟 꼭! 꼭!
+  "ISFP" : 13, // 이달의 예술가
+  "ISFJ" : 14, // 예술가의 예술
+  "ESFJ" : 15, // 영감모임
+  "INFJ" : 16, // 아이디어 공모
+}
+
+const AllBusiness = ({match}) =>{
+    const [char, setChar] = useRecoilState(userState);
+    // const mbtiid = resultData[mbtiMatch]
     const history = useHistory();
     const toHome = () => {
         history.goBack();
+    }
+
+    const onBusinessLink = (data) => {
+      history.push(`/result/${data.mbti}`)
+      console.log(data)
     }
 
     return(
         <>
         <Header>
             <img src={HomeIcon} onClick={toHome} />
-            <img src={HomeIcon} onClick={toHome} />
+            {
+            char[0] == "존중이" ?
+            <TopChar src={Respect} alt='상단로고'/>
+            :
+            char[0] == "상상이" ?
+            <TopChar src={Dream} alt='상단로고'/>
+            :
+            char[0] == "성장이" ?
+            <TopChar src={Expansion} alt='상단로고'/>
+            :
+            <TopChar src={Happy} alt='상단로고'/>
+          }
         </Header>
         <Wrapper>
             <div style={{fontSize:"40px"}}>의정부 사업 유형 목록</div>
@@ -62,7 +106,7 @@ const AllBusiness = ({}) =>{
                 resultData.map((data, idx) => (
                     idx != 0 ?
                         <SuggestBox>
-                            <SuggestSubWrapper>
+                            <SuggestSubWrapper onClick={(e) => {onBusinessLink(data, e)}}>
                                 <SuggestImg src={suggestImg[idx]}/>
                                 <div style={{fontSize:"22px"}}>{data.subject.replace(/<br>/g, ' ')}</div>
                                 <br/>
@@ -131,4 +175,9 @@ const SuggestImg = styled.img`
 const SubjectTxt = styled.span `
   margin-top: 10px;
   font-size: 20px;
+`
+
+const TopChar = styled.img`
+width: 38px;
+margin-right: 10px;
 `
