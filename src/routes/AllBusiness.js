9eb54825e6233ac71fc41b0b04eca81519c1651c
minsu.rob <minsu.rob@gmail.com>
2023-04-01 11:03:06 +0900

    fix: 의정부 사업 유형 목록 이미지 변경

--- src/routes/AllBusiness.js
+++ src/routes/AllBusiness.js
@@ -5,17 +5,45 @@ import EyeIcon from "../components/assets/svg/Eye.svg";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
-import sug1 from "../components/assets/svg/business/01.svg"
 import resultData from '../components/assets/data/resultData';
+
+import business01 from "../components/assets/svg/business/01.svg"
+import business02 from "../components/assets/svg/business/02.svg"
+import business03 from "../components/assets/svg/business/03.svg"
+import business04 from "../components/assets/svg/business/04.svg"
+import business05 from "../components/assets/svg/business/05.svg"
+import business06 from "../components/assets/svg/business/06.svg"
+import business07 from "../components/assets/svg/business/07.svg"
+import business08 from "../components/assets/svg/business/08.svg"
+import business09 from "../components/assets/svg/business/09.svg"
+import business10 from "../components/assets/svg/business/10.svg"
+import business11 from "../components/assets/svg/business/11.svg"
+import business12 from "../components/assets/svg/business/12.svg"
+import business13 from "../components/assets/svg/business/13.svg"
+import business14 from "../components/assets/svg/business/14.svg"
+import business15 from "../components/assets/svg/business/15.svg"
+import business16 from "../components/assets/svg/business/16.svg"
 
 
 const suggestImg = new Array(
-    /*0 */   sug1, 
-    /*1 */   sug1, 
-    /*2 */   sug1,
-    /*3 */   sug1,
-  )
-
+  business01, //index 0
+  business01,
+  business02,
+  business03,
+  business04,
+  business05,
+  business06,
+  business07,
+  business08,
+  business09,
+  business10,
+  business11,
+  business12,
+  business13,
+  business14,
+  business15,
+  business16,
+)
 const AllBusiness = ({}) =>{
     const history = useHistory();
     const toHome = () => {
@@ -35,7 +63,7 @@ const AllBusiness = ({}) =>{
                     idx != 0 ?
                         <SuggestBox>
                             <SuggestSubWrapper>
-                                <SuggestImg src={suggestImg[resultData[0].suggest[0]]}/>
+                                <SuggestImg src={suggestImg[idx]}/>
                                 <div style={{fontSize:"22px"}}>{data.subject.replace('<br>', ' ')}</div>
                                 <br/>
                                 <div style={{fontSize:"22px", textAlign: "center"}}>{data.hashtag}</div>
